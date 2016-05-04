a8c61c422efb54ec8a11398326af1b7f4c9cf64d
andy <dreamrrrr@hotmail.com>
2016-05-04 17:48:32 +0800

    init

---
+++ WebApplication2/Handler1.ashx.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Web;
+
+namespace WebApplication2
+{
+    /// <summary>
+    /// Handler1 的摘要描述
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpFileCollection files = context.Request.Files;
+
+            string id = "id";
+            string name = "name";
+            result rlt = new result() { id = id, name = name };
+            DataContractJsonSerializer json = new DataContractJsonSerializer(rlt.GetType());
+            json.WriteObject(context.Response.OutputStream, rlt);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public class result
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+    }
+}
